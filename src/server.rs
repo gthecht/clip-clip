be351502a02520dd4c711e144334bbc32346b110
gthecht <gdhecht@gmail.com>
2024-09-22 21:40:52 +0300

    cleanup Results inside leftover

--- src/server.rs
+++ src/server.rs
@@ -2,40 +2,11 @@ use crate::{
     leftover::get_coverage,
     types::{CoverResponse, GetCoverBody},
 };
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::post,
-    Json, Router,
-};
-use eyre::Result;
+use axum::{routing::post, Json, Router};
 
-struct ErrorReport(eyre::Report);
-
-impl IntoResponse for ErrorReport {
-    fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("An error occurred: {}", self.0),
-        )
-            .into_response()
-    }
-}
-
-impl<R> From<R> for ErrorReport
-where
-    R: Into<eyre::Report>,
-{
-    fn from(report: R) -> Self {
-        Self(report.into())
-    }
-}
-
-async fn get_coverage_handler(
-    Json(payload): Json<GetCoverBody>,
-) -> Result<Json<CoverResponse>, ErrorReport> {
-    let response = get_coverage(payload.subject, payload.clippers)?;
-    Ok(Json(response))
+async fn get_coverage_handler(Json(payload): Json<GetCoverBody>) -> Json<CoverResponse> {
+    let response = get_coverage(payload.subject, payload.clippers);
+    Json(response)
 }
 
 pub fn create_router() -> Router {
