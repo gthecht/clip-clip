139bcc3384b97d6919df8e12ec5cb7b0ded55e4e
gthecht <gdhecht@gmail.com>
2024-09-23 15:08:53 +0300

    Add health route and get PORT from env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use axum::serve;
 use server::create_router;
 mod leftover;
@@ -7,9 +9,12 @@ mod types;
 #[tokio::main]
 async fn main() {
     let app = create_router();
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let host = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(&host)
         .await
-        .expect("bind to port 3000 failed");
+        .expect(&format!("bind to port {} failed", port));
+    println!("app listening on {}", host);
     serve(listener, app)
         .await
         .expect("failed to serve app on listener");
--- src/server.rs
+++ src/server.rs
@@ -2,13 +2,22 @@ use crate::{
     leftover::get_coverage,
     types::{CoverResponse, GetCoverBody},
 };
-use axum::{routing::post, Json, Router};
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
 
 async fn get_coverage_handler(Json(payload): Json<GetCoverBody>) -> Json<CoverResponse> {
     let response = get_coverage(payload.subject, payload.clippers);
     Json(response)
 }
 
+async fn get_health() -> String {
+    "GOOD".to_string()
+}
+
 pub fn create_router() -> Router {
-    Router::new().route("/coverage", post(get_coverage_handler))
+    Router::new()
+        .route("/coverage", post(get_coverage_handler))
+        .route("/health", get(get_health))
 }
