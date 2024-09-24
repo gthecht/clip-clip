a559f3d6e4fd302d2102a375aa8a2fe9146109c7
gthecht <gdhecht@gmail.com>
2024-09-24 08:05:06 +0300

    Add tracing logger

--- src/server.rs
+++ src/server.rs
@@ -6,6 +6,8 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 
 async fn get_coverage_handler(Json(payload): Json<GetCoverBody>) -> Json<CoverResponse> {
     let response = get_coverage(payload.subject, payload.clippers);
@@ -17,7 +19,18 @@ async fn get_health() -> String {
 }
 
 pub fn create_router() -> Router {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
     Router::new()
         .route("/coverage", post(get_coverage_handler))
         .route("/health", get(get_health))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new()
+                    .level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new()
+                    .level(Level::INFO)),
+        )
 }
